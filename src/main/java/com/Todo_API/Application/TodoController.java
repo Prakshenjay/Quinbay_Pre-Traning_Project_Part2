1a9afdaeb140bddfe5f06163f672dad2705fdcb6
Prakshenjay Bhati <41756887+Prakshenjay@users.noreply.github.com>
2024-12-18 17:51:57 +0530

    Update TodoController.java

--- src/main/java/com/Todo_API/Application/TodoController.java
+++ src/main/java/com/Todo_API/Application/TodoController.java
@@ -1,4 +1,37 @@
 package com.Todo_API.Application;
 
+import com.Todo_API.Application.model.Todo;
+import com.Todo_API.Application.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/todos")
+@CrossOrigin(origins = "*")
 public class TodoController {
+
+    @Autowired
+    private TododoService todoService;
+
+    @GetMapping
+    public List<Todo> getAllTodos() {
+        return todoService.getAllTodos();
+    }
+
+    @PostMapping
+    public Todo createTodo(@RequestBody Todo todo) {
+        return todoService.createTodo(todo);
+    }
+
+    @PostMapping("/{id")
+    public Todo updateTodo(@PathVariable String id, @RequestBody Todo updatedTodo) {
+        return todoService.updateTodo(id, updatedTodo);
+    }
+
+    @DeleteMapping("/{id")
+    public void deleteTodoById(@PathVariable String id) {
+        todoService.deleteTodoById(id);
+    }
 }
