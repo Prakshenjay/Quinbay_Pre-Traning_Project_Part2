4ec463804c428fabe8edb31896bcd63ace05b96c
Prakshenjay Bhati <41756887+Prakshenjay@users.noreply.github.com>
2024-12-20 02:05:10 +0530

    Create TodoService.java

---
+++ Back End/src/main/java/com/Todo_API/Application/service/TodoService.java
@@ -0,0 +1,40 @@
+package com.Todo_API.Application.service;
+
+import com.Todo_API.Application.model.Todo;
+import com.Todo_API.Application.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TodoService {
+
+    @Autowired
+    private TodoRepository todoRepository;
+
+    public List<Todo> getAllTodos() {
+        return todoRepository.findAll();
+    }
+
+    public Optional<Todo> getTodoById(String id) {
+        return todoRepository.findById(id);
+    }
+
+    public Todo createTodo(Todo todo) {
+        return todoRepository.save(todo);
+    }
+
+    public Todo updateTodo(String id, Todo updatedTodo) {
+        return todoRepository.findById(id).map(todo -> {
+            todo.setText(updatedTodo.getText());
+            todo.setCompleted(updatedTodo.isCompleted());
+            return todoRepository.save(todo);
+        }).orElseThrow( () -> new RuntimeException("Todo not found with Id" + id));
+    }
+
+    public void deleteTodoById(String id) {
+        todoRepository.deleteById(id);
+    }
+}
