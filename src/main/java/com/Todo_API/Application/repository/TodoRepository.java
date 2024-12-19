c88fab5b96f81d22d511ec0dd42e11e4c139a7f0
Prakshenjay Bhati <41756887+Prakshenjay@users.noreply.github.com>
2024-12-20 02:05:36 +0530

    Delete TodoRepository.java

--- src/main/java/com/Todo_API/Application/repository/TodoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Todo_API.Application.repository;
-
-import com.Todo_API.Application.model.Todo;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TodoRepository extends MongoRepository<Todo, String> {
-}
